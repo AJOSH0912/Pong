f263e9ce53171d89ba25a492f1e7682affc2093f
AJOSH0912 <aryan.joshi@education.nsw.gov.au>
2024-07-27 14:54:39 +1000

    2nd hour

--- Pong.py
+++ Pong.py
@@ -47,3 +47,42 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        # Keydown events
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                left_paddle_speed = -PADDLE_SPEED
+            elif event.key == pygame.K_s:
+                left_paddle_speed = PADDLE_SPEED
+            elif event.key == pygame.K_UP:
+                right_paddle_speed = -PADDLE_SPEED
+            elif event.key == pygame.K_DOWN:
+                right_paddle_speed = PADDLE_SPEED
+        # Keyup events
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_w or event.key == pygame.K_s:
+                left_paddle_speed = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                right_paddle_speed = 0
+    
+    # Move paddles
+    left_paddle.y += left_paddle_speed
+    right_paddle.y += right_paddle_speed
+
+    # Prevent paddles from moving out of the screen
+    if left_paddle.top < 0:
+        left_paddle.top = 0
+    if left_paddle.bottom > SCREEN_HEIGHT:
+        left_paddle.bottom = SCREEN_HEIGHT
+    if right_paddle.top < 0:
+        right_paddle.top = 0
+    if right_paddle.bottom > SCREEN_HEIGHT:
+        right_paddle.bottom = SCREEN_HEIGHT
+
+    # Move ball
+    ball.x += BALL_SPEED_X
+    ball.y += BALL_SPEED_Y
+
+    # Ball collision with top/bottom walls
+    if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:
+        BALL_SPEED_Y = -BALL_SPEED_Y
